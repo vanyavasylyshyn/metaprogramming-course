b9198896eab088afdf6ecdaae123f5d310d06fb6
Ivan Vasylyshyn <ivanvasylyshyn@Ivans-iMac-2.local>
2022-02-05 13:29:44 +0200

    Finisted 3rd task

--- task_3.rb
+++ task_3.rb
@@ -1,22 +1,41 @@
 puts 'Exercise: Build custom DSL to generate HTML'
 
-class HTML
-  def initialize
+class HTML < BasicObject
+  def initialize(&block)
+     @html = ""
 
+     instance_eval(&block)
   end
 
   def to_s
+    @html
+  end
 
+  private
+
+  def method_missing(tag, *args, &block)
+    properties = ""
+
+    properties = args.shift.map { |p| p.join("=") } * " " if args.first.is_a? ::Object::Hash
+
+    @html << "<#{tag} #{properties}>"
+    if block
+      instance_eval(&block)
+    else
+      @html << "#{args.first}"
+    end
+    @html << "</#{tag}>"
   end
 end
 
 html = HTML.new do
   h2 "Why is Arbre awesome?"
 
-  ul do
+  ul id: "nav", class: "list" do
     li "The DOM is implemented in ruby"
     li "You can create object oriented views"
     li "Templates suck"
+    p "Same shiiiit every day"
   end
 end
 
